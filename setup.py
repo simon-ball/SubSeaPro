17bef8ba161df0e716f07018e8f8316f6a562598
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2019-06-21 15:27:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author="Simon Ball",
     author_email="simon.ball@ntnu.no",
     description="Server/client for queuing jobs onto a calculation server",
-    long_description=readme
+    long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/simon-ball/SubSeaPro",
 	classifiers=[
