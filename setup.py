32c024e614d43452110692c5be1b95a9ae405779
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2019-06-21 15:24:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPLv3",
         "Operating System :: OS Independent",
-    ]
+    ],
 	install_requires=requirements,
     packages=setuptools.find_packages(),
     
